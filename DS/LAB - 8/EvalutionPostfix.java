79198c28c78670a61eda7d653d56784689dbe124
sumitpipariya <146057368+sumitpipariya@users.noreply.github.com>
2024-07-23 10:58:05 +0530

    Add files via upload

---
+++ DS/LAB - 8/EvalutionPostfix.java
@@ -0,0 +1,53 @@
+//Write a program for evaluation of postfix Expression using Stack.
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class EvalutionPostfix{
+	public static void main(String[] args) {
+		
+		Stack<Integer> stk = new Stack<>();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enetr the infix String : ");
+		String temp = sc.next();
+		int value = 0;
+		temp = temp + " ";
+
+		while(temp.charAt(value)!= ' '){
+			if(Character.isDigit(temp.charAt(value))){
+				stk.push(Integer.parseInt(""+temp.charAt(value)));
+				
+			}
+			else{
+				int operand2 = stk.pop();
+				int operand1 = stk.pop();
+				int result=0;
+
+				if(temp.charAt(value) == '+'){
+					result = operand2 + operand1;
+				}
+				else if(temp.charAt(value) == '-'){
+					result = operand1 - operand2;
+				}
+				else if(temp.charAt(value) == '*'){
+					result = operand1 * operand2;
+				}
+				else if(temp.charAt(value) == '/'){
+					result = operand1 / operand2;
+				}
+				else if(temp.charAt(value) == '^'){
+					result = (int)Math.pow( operand1,operand2);
+				}
+				else{
+					System.out.println("Invalid String");
+				}
+				stk.push(result);
+
+			}
+			value++;
+		}
+
+		System.out.println(stk.pop());
+
+	}
+}
