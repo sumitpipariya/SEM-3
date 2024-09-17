120ae1c9279e4d6f8c146990e026ea1c4221017c
sumitpipariya <146057368+sumitpipariya@users.noreply.github.com>
2024-09-17 09:46:07 +0530

    Add files via upload

---
+++ DS/LAB - 20/RedixSort.java
@@ -0,0 +1,32 @@
+import java..util.Scanner;
+
+public class RedixSort{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the size of array : ");
+		int n = sc.nextInt();
+
+		int arr[] = new int[n];
+		int count[] = new int[n];
+
+		for(int i = 0 ;i<n;i++){
+			arr[i] = sc.nextInt();
+		}
+
+		for(int i = 0;i<n;i++){
+			count[i] = 0;
+		}
+
+		for(int i = 0;i<n;i++){
+			count[arr[i]/]
+		}
+
+	}
+
+	public static void getMex(){
+		
+	}
+
+}
