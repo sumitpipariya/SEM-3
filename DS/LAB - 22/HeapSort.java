c2c1546ed27e453d1a4a291341f71ed80f7ea5cb
sumitpipariya <146057368+sumitpipariya@users.noreply.github.com>
2024-09-17 09:47:05 +0530

    Add files via upload

---
+++ DS/LAB - 22/HeapSort.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HeapSort{
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Entre the size of array : ");
+		int n = sc.nextInt();
+
+		int arr[] = new int[n];
+		for(int i = 0; i > n; i ++){
+			arr[i] = sc.nextInt();
+		}
+
+	}
+
+	public void heapify(int arr[], int n, int index){
+
+	}
+
+}
